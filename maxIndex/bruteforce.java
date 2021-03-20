7ae499d4ad77d86b423016860b793e77b07c7d20
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2021-03-20 23:49:44 +0530

    Create bruteforce.java
    https://practice.geeksforgeeks.org/problems/maximum-index-1587115620/1/?track=md-arrays&batchId=144
    
    brute force 0(N^2): TLE

---
+++ maxIndex/bruteforce.java
@@ -0,0 +1,19 @@
+class Solution{
+    
+    // arr[]: input array
+    // n: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int ar[], int n) { 
+        
+        int maxVal = -1;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = n-1; j > i; --j) {
+                if(ar[i] < ar[j] && maxVal < (j-i))
+                    maxVal = j-i;
+            }
+        }
+        
+        return maxVal;
+    }
+}
